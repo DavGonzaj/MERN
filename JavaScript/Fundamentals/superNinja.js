a9666f5cb6643c54f98bef083468f089d39a4927
DavGonzaj <davgonzaj@gmail.com>
2023-05-02 14:06:18 -0700

    MERN

---
+++ JavaScript/Fundamentals/superNinja.js
@@ -0,0 +1,42 @@
+class Ninja {
+  constructor(name, health = 3, speed = 3, strength = 3) {
+    this.name = name;
+    this.health = health;
+    this.speed = speed;
+    this.strength = strength;
+  }
+
+  sayName() {
+    console.log(`My name is ${this.name}`);
+  }
+
+  showStats() {
+    console.log(this);
+  }
+
+  drinkSake() {
+    this.health += 10;
+    console.log("Health is now: ", this.health);
+  }
+}
+
+class Sensei extends Ninja {
+  constructor(name, wisdom = 10) {
+    super(name, 200, 10, 10, wisdom);
+  }
+
+  speakWisdom() {
+    super.drinkSake();
+    console.log(
+      "What one programmer can do in one month, two programmers can do in two months."
+    );
+  }
+
+  showStats() {
+    super.showStats();
+  }
+}
+
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+superSensei.showStats();
