33bd0d02f444b4aeff5508438348b11231442f9b
DavGonzaj <davgonzaj@gmail.com>
2023-05-13 10:36:49 -0700

    jokes api

--- MongoDB/Jokes_API/server/config/mongoose.config.js
+++ MongoDB/Jokes_API/server/config/mongoose.config.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const username = process.env.ATLAS_USERNAME;
-const pw = process.env.PASSWORD;
+const pw = process.env.ATLAS_PASSWORD;
 const dbName = "jokesdb";
 
 const uri = `mongodb+srv://${username}:${pw}@cluster0.uybyvcv.mongodb.net/${dbName}?retryWrites=true&w=majority`;
